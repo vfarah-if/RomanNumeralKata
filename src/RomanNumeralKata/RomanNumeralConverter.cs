8e8d2b50b4b9cb9513b233c80981cad14bce6141
vfarah-if <vincent.farah@codurance.com>
2019-02-08 15:08:05 +0000

    Refactored for repition code issues

--- src/RomanNumeralKata/RomanNumeralConverter.cs
+++ src/RomanNumeralKata/RomanNumeralConverter.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace RomanNumeralKata
@@ -8,12 +7,12 @@ namespace RomanNumeralKata
     {
         private readonly Dictionary<int, string> romanNumerals = new Dictionary<int, string>
         {
-            {1, "I"},
-            {4, "IV"},
-            {5, "V"},
-            {9, "IX"},
-            {10, "X"},
             {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"},
         };
 
         public string Convert(int arabicNumber)
@@ -24,36 +23,15 @@ namespace RomanNumeralKata
             }
 
             var result = new StringBuilder();
-            while(arabicNumber >= 40)
+            foreach (var romanNumeral in romanNumerals)
             {
-                result.Append("XL");
-                arabicNumber -= 40;
+                while (arabicNumber >= romanNumeral.Key)
+                {
+                    result.Append(romanNumeral.Value);
+                    arabicNumber -= romanNumeral.Key;
+                }
             }
-
-            while (arabicNumber >= 10)
-            {
-                result.Append("XL");
-                arabicNumber -= 10;
-            }
-
-            while (arabicNumber >= 5)
-            {
-                result.Append("V");
-                arabicNumber -= 5;
-            }
-
-            while (arabicNumber >= 4)
-            {
-                result.Append("IV");
-                arabicNumber -= 4;
-            }
-
-            while (arabicNumber >= 1)
-            {
-                result.Append("I");
-                arabicNumber -= 1;
-            }
-
+          
             return result.ToString();
         }
     }
