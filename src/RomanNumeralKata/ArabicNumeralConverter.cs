36c910f59c3c7ff8ad8a0a1dc8cb459de4a0fb1a
vfarah-if <vincent.farah@codurance.com>
2019-02-08 15:37:50 +0000

    Created first failing test for Arabic Numeral Co

---
+++ src/RomanNumeralKata/ArabicNumeralConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanNumeralKata
+{
+    public class ArabicNumeralConverter
+    {
+        public int Convert(string romanNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/RomanNumeralKata.UnitTests/ArabicNumeralConverterShould.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RomanNumeralKata.UnitTests
+{
+    public class ArabicNumeralConverterShould
+    {
+        private ArabicNumeralConverter arabicNumeralCoverter;
+
+        public ArabicNumeralConverterShould()
+        {
+            arabicNumeralCoverter = new ArabicNumeralConverter();
+        }
+
+        [Theory]
+        [InlineData("I", 1)]
+        public void ConvertRomanNumeralToArabic(string romanNumber, int expected)
+        {
+            var actual = arabicNumeralCoverter.Convert(romanNumber);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
