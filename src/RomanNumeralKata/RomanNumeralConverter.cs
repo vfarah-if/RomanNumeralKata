96bcaa6c353faecb49fc6fd0f0c5fd499046908b
vfarah-if <vincent.farah@codurance.com>
2019-02-08 15:20:12 +0000

    Got all tests passing

--- src/RomanNumeralKata/RomanNumeralConverter.cs
+++ src/RomanNumeralKata/RomanNumeralConverter.cs
@@ -7,6 +7,13 @@ namespace RomanNumeralKata
     {
         private readonly Dictionary<int, string> romanNumerals = new Dictionary<int, string>
         {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
             {40, "XL"},
             {10, "X"},
             {9, "IX"},
